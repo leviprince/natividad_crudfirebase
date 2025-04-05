d2c0de25a18ec34d4ae39b7680e51f0f1f061f72
leviprince <saltytorra@gmail.com>
2025-04-05 11:06:14 +0800

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,88 @@
+var rollV, nameV, genderV, addressV, zodiacV, idnoV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  zodiacV = document.getElementById("zodiac").value;
+  idnoV = document.getElementById("idno").value;
+  console.log(rollV, nameV, addressV, genderV, zodiacV, idnoV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      zodiac: zodiacV,
+      idno: idnoV,
+    });
+  alert("Data Inserted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("zodiac").value = "";
+  document.getElementById("idno").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
+      document.getElementById("zodiac").value = snap.val().zodiac;
+      document.getElementById("idno").value = snap.val().idno;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      zodiac: zodiacV,
+      idno: idnoV,
+    });
+  alert("Data Update");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("zodiac").value = "";
+  document.getElementById("idno").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("zodiac").value = "";
+  document.getElementById("idno").value = "";
+};
