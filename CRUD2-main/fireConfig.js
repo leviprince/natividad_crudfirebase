97fa0bce89b10accae953edd10a79dd3dc499315
leviprince <saltytorra@gmail.com>
2025-04-05 11:05:47 +0800

    Add files via upload

---
+++ CRUD2-main/fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCOVtvMBTkEhTzjbbe16xd6-DXA8dZ33MY",
+  authDomain: "natividad-mid1.firebaseapp.com",
+  databaseURL: "https://natividad-mid1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "natividad-mid1",
+  storageBucket: "natividad-mid1.firebasestorage.app",
+  messagingSenderId: "171839294547",
+  appId: "1:171839294547:web:39ff4bd618b5f1c4594aa6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
